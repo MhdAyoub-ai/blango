dd974dba12ab9276d7c673484ae6423c414e12b1
Muhammad Ayoub <muha94ma@gmail.com>
2023-08-04 11:52:04 +0000

    Finish API Authontication

--- blango/settings.py
+++ blango/settings.py
@@ -40,7 +40,13 @@ class Dev(Configuration):
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
     CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-
+    REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
 
     # Application definition
 
@@ -62,6 +68,7 @@ class Dev(Configuration):
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
         'rest_framework',
+        'rest_framework.authtoken',
 
     ]
     SITE_ID = 1
